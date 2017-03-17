e0c7d275d0a26482e7e3c57cac107aba7dd742c0
ntphong1990 <ntphong1990@gmail.com>
2017-03-17 16:59:10 +0700

    as

--- app/Controller/BackEndController.php
+++ app/Controller/BackEndController.php
@@ -10,6 +10,7 @@ class BackEndController extends AppController {
 
 
     public function getProduct(){
+
         $this->loadModel('Product');
 
         $this->Paginator = $this->Components->load('Paginator');
@@ -35,4 +36,74 @@ class BackEndController extends AppController {
         return json_encode($products);
     }
 
+    public function order(){
+
+        $this->loadModel('Order');
+        $this->loadModel('Customer');
+        $this->loadModel('OrderItem');
+        $customer = $this->Customer->find('first',array('conditions' => array(
+            'id' => $this->request->data['customerid']
+        )));
+        $order = $this->Order->create();
+        $order['Order']['first_name'] = $customer['Customer']['name'];
+        $order['Order']['customer_id'] = $customer['Customer']['id'];
+        $order['Order']['last_name'] = $customer['Customer']['lastname'];
+        $order['Order']['email'] = $customer['Customer']['email'];
+        $order['Order']['phone'] = $customer['Customer']['phone'];
+        $order['Order']['billing_address'] = $customer['Customer']['address'];
+        $order['Order']['billing_address2'] = '';
+        $order['Order']['billing_city'] = '';
+        $order['Order']['note'] = '';
+        $order['Order']['direct'] = '1';
+        $order['Order']['shipping_address'] = '';
+        $order['Order']['shipping_city'] = '';
+        $order['Order']['order_type'] = 'direct';
+        $order['Order']['order_item_count'] = 1;
+        $order['Order']['quantity'] = 1;
+        $order['Order']['weight'] = '0';
+        $order['Order']['subtotal'] = $this->request->data['subtotal'];
+        $order['Order']['voucher'] = 0;
+        $order['Order']['code'] = '';
+        $order['Order']['discount'] = 0;
+        $order['Order']['note'] = $this->request->data['note'];
+        $order['Order']['shipping'] = $this->request->data['shipping'];
+        $order['Order']['total'] = $this->request->data['total'];
+        $order['Order']['status'] = $this->request->data['status'];
+
+        $orderItems = json_decode($this->request->data['orderitem']);
+        $order['OrderItem'] = array();
+        $weight = 0;
+        foreach ($orderItems as $key => $value){
+            $orderItem = array();
+
+            $orderItem['product_id'] = $value->id;
+            $orderItem['name'] = $value->name;
+            $orderItem['weight'] = $value->weight * $value->quanlity;
+            $orderItem['price'] = $value->price;
+            $orderItem['quantity'] = $value->quanlity;
+            $orderItem['subtotal'] = $value->quanlity * $value->price;
+            $weight = $weight + $orderItem['weight'];
+            array_push($order['OrderItem'],$orderItem);
+        }
+        if($order['Order']['status'] == 1) {
+            $order['Financial'] = array();
+            $fee = array();
+            $fee['type'] = 1;
+            $fee['value'] = $order['Order']['total'];
+            $fee['note'] = 'Đơn đặt hàng từ ' . $customer['Customer']['name'];
+            $fee['kind'] = 1;
+            array_push($order['Financial'], $fee);
+        }
+        $order['Order']['weight'] = $weight;
+        // var_dump($order);die();
+      //  $a = $this->Order->saveAll($order);
+        $a = $this->Order->saveAll($order);
+
+            $this->loadModel('Log');
+            $this->Log->newOrderApp($this->Order->inserted_ids[0]);
+            $this->Order->set($order);
+
+            return json_encode(true);
+
+    }
 }
