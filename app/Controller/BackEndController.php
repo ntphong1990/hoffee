38e65296d7bc045bbf195e2cc2b326e3e5267214
ntphong1990 <ntphong1990@gmail.com>
2017-04-11 15:18:39 +0700

    login

--- app/Controller/BackEndController.php
+++ app/Controller/BackEndController.php
@@ -1,12 +1,15 @@
 <?php
-class BackEndController extends AppController {
+class BackEndController extends AppController
+{
 
-    public function beforeFilter() {
+    public function beforeFilter()
+    {
         $this->Auth->allow();
         $this->autoRender = false;
     }
 
-    public function getProduct(){
+    public function getProduct()
+    {
 
         $this->loadModel('Product');
 
@@ -30,18 +33,18 @@ class BackEndController extends AppController {
         // );
 
         $this->Paginator->settings = array(
-                'recursive' => -1,
-                'limit' => 20,
-                'order' => array(
-                    'name' => 'ASC'
-                ),
-                'paramType' => 'querystring',
+            'recursive' => -1,
+            'limit' => 20,
+            'order' => array(
+                'name' => 'ASC'
+            ),
+            'paramType' => 'querystring',
         );
 
         $products = $this->Paginator->paginate('Product');
         $listProduct = array();
 
-        for ($i = 0; $i < count($products); $i++) { 
+        for ($i = 0; $i < count($products); $i++) {
             array_push($listProduct, $products[$i]['Product']);
         }
 
@@ -49,18 +52,19 @@ class BackEndController extends AppController {
     }
 
 
-    public function getCities(){
+    public function getCities()
+    {
         $this->loadModel('DevvnTinhthanhpho');
         $this->loadModel('DevvnQuanhuyen');
 
         $cities = $this->DevvnTinhthanhpho->find('all');
         $listCities = array();
 
-        for ($i = 0 ; $i < count($cities) ; $i++) {
+        for ($i = 0; $i < count($cities); $i++) {
             $city = $cities[$i]['DevvnTinhthanhpho'];
-            $states = $this->DevvnQuanhuyen->find('all',array('conditions' => array('matp' => $cities[$i]['DevvnTinhthanhpho']['matp'])));
+            $states = $this->DevvnQuanhuyen->find('all', array('conditions' => array('matp' => $cities[$i]['DevvnTinhthanhpho']['matp'])));
             $listState = array();
-            for ($j = 0; $j < count($states); $j++) { 
+            for ($j = 0; $j < count($states); $j++) {
                 $state = $states[$j]['DevvnQuanhuyen'];
                 array_push($listState, $state);
             }
@@ -71,18 +75,19 @@ class BackEndController extends AppController {
         return json_encode($listCities);
     }
 
-    public function orderVer1(){
+    public function orderVer1()
+    {
 
-        $jsonData=$this->request->input('json_decode', true );
-        
+        $jsonData = $this->request->input('json_decode', true);
+
         $this->loadModel('Customer');
         $customer = null;
 
-        $isCustomerExisted = $this->Customer->find('first',array('conditions' => array(
+        $isCustomerExisted = $this->Customer->find('first', array('conditions' => array(
             'phone' => $this->request->data['phone']
         )));
 
-        if(!$isCustomerExisted){
+        if (!$isCustomerExisted) {
             $customer = $this->Customer->create();
             $customer['Customer']['name'] = $jsonData['first_name'];
             $customer['Customer']['lastname'] = $jsonData['last_name'];
@@ -91,14 +96,14 @@ class BackEndController extends AppController {
             $customer['Customer']['phone'] = $jsonData['phone'];
             $customer['Customer']['birthday'] = date('Y-m-d');
             $customer['Customer']['district'] = $jsonData['billing_city'];
-            $customer['Customer']['state'] =  $jsonData['state'];
+            $customer['Customer']['state'] = $jsonData['state'];
 
             $this->Customer->save($customer);
-      
+
             $customer['Customer']['id'] = $this->Customer->inserted_ids[0];
 
         } else {
-            $customer = $this->Customer->find('first',array('conditions' => array(
+            $customer = $this->Customer->find('first', array('conditions' => array(
                 'phone' => $this->request->data['phone']
             )));
         }
@@ -115,8 +120,8 @@ class BackEndController extends AppController {
         $order['Order']['phone'] = $customer['Customer']['phone'];
         $order['Order']['billing_address'] = $jsonData['billing_address'];
         $order['Order']['billing_address2'] = '';
-        $city = $this->DevvnTinhthanhpho->find('first',array('conditions' => array('matp' => $customer['Customer']['district'])));
-        if($city) {
+        $city = $this->DevvnTinhthanhpho->find('first', array('conditions' => array('matp' => $customer['Customer']['district'])));
+        if ($city) {
             $order['Order']['billing_city'] = $city['DevvnTinhthanhpho']['name'];
         } else {
             $order['Order']['billing_city'] = '';
@@ -141,7 +146,7 @@ class BackEndController extends AppController {
         $orderItems = json_decode($jsonData['orderitem']);
         $order['OrderItem'] = array();
         $weight = 0;
-        foreach ($orderItems as $key => $value){
+        foreach ($orderItems as $key => $value) {
             $orderItem = array();
 
             $orderItem['product_id'] = $value->id;
@@ -151,9 +156,9 @@ class BackEndController extends AppController {
             $orderItem['quantity'] = $value->quanlity;
             $orderItem['subtotal'] = $value->quanlity * $value->price;
             $weight = $weight + $orderItem['weight'];
-            array_push($order['OrderItem'],$orderItem);
+            array_push($order['OrderItem'], $orderItem);
         }
-        if($order['Order']['status'] == 1) {
+        if ($order['Order']['status'] == 1) {
             $order['Financial'] = array();
             $fee = array();
             $fee['type'] = 1;
@@ -176,13 +181,14 @@ class BackEndController extends AppController {
     }
 
 
-    public function order(){
+    public function order()
+    {
 
         $this->loadModel('Order');
         $this->loadModel('Customer');
         $this->loadModel('OrderItem');
         $this->loadModel('DevvnTinhthanhpho');
-        $customer = $this->Customer->find('first',array('conditions' => array(
+        $customer = $this->Customer->find('first', array('conditions' => array(
             'id' => $this->request->data['customerid']
         )));
         $order = $this->Order->create();
@@ -193,8 +199,8 @@ class BackEndController extends AppController {
         $order['Order']['phone'] = $customer['Customer']['phone'];
         $order['Order']['billing_address'] = $customer['Customer']['address'];
         $order['Order']['billing_address2'] = '';
-        $city = $this->DevvnTinhthanhpho->find('first',array('conditions' => array('matp' => $customer['Customer']['district'])));
-        if($city) {
+        $city = $this->DevvnTinhthanhpho->find('first', array('conditions' => array('matp' => $customer['Customer']['district'])));
+        if ($city) {
             $order['Order']['billing_city'] = ['DevvnTinhthanhpho']['name'];
         } else {
             $order['Order']['billing_city'] = '';
@@ -219,7 +225,7 @@ class BackEndController extends AppController {
         $orderItems = json_decode($this->request->data['orderitem']);
         $order['OrderItem'] = array();
         $weight = 0;
-        foreach ($orderItems as $key => $value){
+        foreach ($orderItems as $key => $value) {
             $orderItem = array();
 
             $orderItem['product_id'] = $value->id;
@@ -229,9 +235,9 @@ class BackEndController extends AppController {
             $orderItem['quantity'] = $value->quanlity;
             $orderItem['subtotal'] = $value->quanlity * $value->price;
             $weight = $weight + $orderItem['weight'];
-            array_push($order['OrderItem'],$orderItem);
+            array_push($order['OrderItem'], $orderItem);
         }
-        if($order['Order']['status'] == 1) {
+        if ($order['Order']['status'] == 1) {
             $order['Financial'] = array();
             $fee = array();
             $fee['type'] = 1;
@@ -242,19 +248,20 @@ class BackEndController extends AppController {
         }
         $order['Order']['weight'] = $weight;
         // var_dump($order);die();
-      //  $a = $this->Order->saveAll($order);
+        //  $a = $this->Order->saveAll($order);
         $a = $this->Order->saveAll($order);
 
-            $this->loadModel('Log');
-            $this->Log->newOrderApp($this->Order->inserted_ids[0]);
-            $this->Order->set($order);
+        $this->loadModel('Log');
+        $this->Log->newOrderApp($this->Order->inserted_ids[0]);
+        $this->Order->set($order);
 
-            return json_encode(true);
+        return json_encode(true);
 
     }
 
 
-    public function getUserInfo($phone){
+    public function getUserInfo($phone)
+    {
         //$phone = $this->request->data['phone'];
         $this->loadModel('Customer');
         $customer = $this->Customer->find('first', array('conditions' => array('phone' => $phone)));
@@ -263,7 +270,166 @@ class BackEndController extends AppController {
     }
 
 
-    public function searchCustomer($key){
-        
+    public function searchCustomer($key)
+    {
+
+    }
+
+
+    public function getPassword()
+    {
+        $jsonData = $this->request->input('json_decode', true);
+        $response['result'] = 1;
+        $response['code'] = "OK";
+        $response['data'] = null;
+        $email = $jsonData['email'];
+
+        $this->loadModel('Customer');
+        $customer = $this->Customer->find('first', array('conditions' => array('email' => $email)));
+        $pass = '';
+        if ($customer) {
+            //$pass = $this->generateRandomString();
+            $pass = '123456';
+            $customer['Customer']['password'] = AuthComponent::password($pass);
+
+            $this->Customer->save($customer);
+            $customer['Customer']['password'] = $pass;
+            App::uses('CakeEmail', 'Network/Email');
+            $email = new CakeEmail('gmail');
+            $email->from(array(Configure::read('Settings.ADMIN_EMAIL') => 'Hoffee'))
+                ->cc(Configure::read('Settings.ADMIN_EMAIL'))
+                ->cc('order@hoffee.vn')
+                ->to($customer['Customer']['email'])
+                ->subject('Hoffee - Get Password')
+                ->template('password')
+                ->emailFormat('html')
+                ->viewVars(array('customer' => $customer))
+                ->send();
+            $response['data'] = $customer;
+        } else {
+            $response['result'] = 0;
+            $response['code'] = "EMAIL_INVALID";
+            $response['data'] = null;
+        }
+//UD5MEB
+
+        return json_encode($response);
+    }
+
+    public function register()
+    {
+        $jsonData = $this->request->input('json_decode', true);
+        $this->loadModel('Customer');
+        $customer = null;
+
+        $response = array();
+        $response['result'] = 1;
+        $response['code'] = "OK";
+        $response['data'] = null;
+        $isCustomerExisted = $this->Customer->find('first', array('conditions' => array(
+            'phone' => $this->request->data['phone']
+        )));
+
+        if (!$isCustomerExisted) {
+            $customer = $this->Customer->create();
+            $customer['Customer']['name'] = $jsonData['first_name'];
+            $customer['Customer']['lastname'] = $jsonData['last_name'];
+            $customer['Customer']['address'] = $jsonData['billing_address'];
+            $customer['Customer']['email'] = $jsonData['email'];
+            $customer['Customer']['phone'] = $jsonData['phone'];
+            $customer['Customer']['birthday'] = date('Y-m-d');
+            $customer['Customer']['district'] = $jsonData['billing_city'];
+            $customer['Customer']['state'] = $jsonData['state'];
+
+            $this->Customer->save($customer);
+
+            $customer['Customer']['id'] = $this->Customer->inserted_ids[0];
+            $response['data'] = $customer;
+        } else {
+            $response['result'] = 0;
+            $response['code'] = "EXISTED";
+        }
+        return json_encode($response);
+    }
+
+
+    public function login()
+    {
+        $jsonData = $this->request->input('json_decode', true);
+        $email = $jsonData['email'];
+        $pass = $jsonData['password'];
+
+        $response['result'] = 1;
+        $response['code'] = "OK";
+        $response['data'] = null;
+        $this->loadModel('Customer');
+        $customer = $this->Customer->find('first', array('conditions' => array(
+            'email' => $email,
+            'password' => AuthComponent::password($pass)
+        )));
+
+        if ($customer) {
+            $customer['Customer']['token'] = $this->getToken();
+            $this->Customer->save($customer);
+            $response['data'] = $customer;
+        } else {
+            $response['result'] = 0;
+            $response['code'] = "LOGIN_INVALID";
+            $response['data'] = null;
+        }
+        return json_encode($response);
+
+    }
+
+    public function me(){
+
+        $response['result'] = 1;
+        $response['code'] = "OK";
+        $response['data'] = null;
+        $jsonData = $this->request->input('json_decode', true);
+        $token = $jsonData['token'];
+        $this->loadModel('Customer');
+        $customer = $this->Customer->find('first', array('conditions' => array(
+            'token' => $token
+        )));
+        if ($customer) {
+            $customer['Customer']['token'] = $this->getToken();
+            $this->Customer->save($customer);
+            $response['data'] = $customer;
+        } else {
+            $response['result'] = 0;
+            $response['code'] = "TOKEN_EXPIRED";
+            $response['data'] = null;
+        }
+
+        return json_encode($response);
+    }
+
+    function generateRandomString($length = 6)
+    {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    function getToken($length = 50)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
+
+
+
+
+
+
---
+++ app/View/Emails/html/password.ctp
@@ -0,0 +1,73 @@
+<div id=":20h" class="ii gt adP adO"><div id=":20g" class="a3s aXjCH m1587c2e06f1d3f66"><u></u>
+
+
+
+
+
+
+
+        <div bgcolor="" link="#c6d4df" alink="#9DB7D0" vlink="#6d93b8" text="#d7d7d7" style="font-family:Helvetica,Arial,sans-serif;font-size:14px;color:#d7d7d7">
+            <table style="width:538px;background-color:#393836" align="center" cellspacing="0" cellpadding="0" border="0">
+                <tbody><tr>
+                    <td style="height:65px;background-color:#000000;border-bottom:1px solid #4d4b48">
+                        <a  href="http://hoffee.vn">
+                            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 110 30" style="width: 110px; height: 30px;" width="110" height="30"><defs><style>.cls-1{fill:#0a4e9b;}</style></defs><title>Artboard 1</title><path class="cls-1" d="M16.43,25V15.33H7.72V25H5V5H5A2.73,2.73,0,0,1,7.72,7.73V13h8.71V7.73A2.73,2.73,0,0,1,19.15,5h0V25Z"></path><path class="cls-1" d="M24.78,7.53V22.47A2.52,2.52,0,0,0,27.3,25h7.84a2.52,2.52,0,0,0,2.51-2.52V7.53A2.52,2.52,0,0,0,35.14,5H27.3A2.52,2.52,0,0,0,24.78,7.53ZM27.5,21.35V7.46h6a1.47,1.47,0,0,1,1.47,1.47V21.24a1.3,1.3,0,0,1-1.3,1.3H28.69A1.19,1.19,0,0,1,27.5,21.35Z"></path><path class="cls-1" d="M46.06,7.46V13h7a2.32,2.32,0,0,1-2.31,2.32H46.06V25H43.34V5H55.65A2.45,2.45,0,0,1,53.2,7.46Z"></path><path class="cls-1" d="M62,7.46V13h7a2.32,2.32,0,0,1-2.31,2.32H62V25H59.27V5H71.58a2.45,2.45,0,0,1-2.45,2.46Z"></path><path class="cls-1" d="M78.85,7.46V13h6.58a2.32,2.32,0,0,1-2.31,2.32H78.85v7.21h9V25H76.13V5H88a2.45,2.45,0,0,1-2.45,2.46Z"></path><path class="cls-1" d="M95.82,7.46V13h6.58a2.32,2.32,0,0,1-2.31,2.32H95.82v7.21h9V25H93.1V5H105a2.45,2.45,0,0,1-2.45,2.46Z"></path></svg>
+
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#17212e">
+                        <table width="470" border="0" align="center" cellpadding="0" cellspacing="0" style="padding-left:5px;padding-right:5px;padding-bottom:10px">
+                            <tbody><tr bgcolor="#17212e">
+                                <td style="padding-top:32px">
+					<span style="font-family:Helvetica,Arial,sans-serif;font-size:24px;color:#66c0f4;font-weight:bold">
+						Hello <?php echo $customer['Customer']['name'];?>				</span><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="font-family:Helvetica,Arial,sans-serif;font-size:14px;color:#c6d4df;padding-top:16px">
+                                    <h3 style="margin-bottom:0px">Your Hoffee's password is <?php echo $customer['Customer']['password'];?></h3>
+                                    			   </td>
+                            </tr>
+                            <tr><td style="padding-top:10px"></td></tr>
+
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="font-family:Helvetica,Arial,sans-serif;font-size:12px;color:#61696d;padding-top:16px">
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="font-family:Helvetica,Arial,sans-serif;font-size:12px;color:#61696d;padding-top:20px">
+                                    The <span class="il">Hoffee</span> Support Team<br>
+                                </td>
+                            </tr>
+                            </tbody></table>
+                    </td>
+                </tr>
+                <tr style="background-color:#17212e">
+                    <td style="padding-top:50px">
+                    </td>
+                </tr>
+                <tr style="background-color:#000000">
+                    <td style="padding:12px 24px">
+                        <table cellpadding="0" cellspacing="0">
+                            <tbody><tr>
+                                <td width="92">
+                                    <img src="https://ci4.googleusercontent.com/proxy/IHzYs4Fk-P7UZ1YcB6Tkq65fai9JnoAT2ahgIjsunSydPWxVzc4vDpTCbkBmYdfeceZJ8fQM3uZrw-QC-5ZYvRjYVNjvREOLTSj5NLgv_wyGVt5cuj0OtEp934Ta=s0-d-e1-ft#http://store.akamai.steamstatic.com/public/images/logo_valve_footer.jpg" width="92" height="26" alt="Valve®" class="CToWUd">
+                                </td>
+                                <td style="font-size:11px;color:#595959;padding-left:12px">
+                                   			</td>
+                            </tr>
+                            </tbody></table>
+                    </td>
+                </tr>
+                </tbody>
+                </table>
+
+
+
+        </div></div></div>
